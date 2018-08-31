0be9c481a022995d6c60e58eba279c8e9c277a3a
Kristie Desiree <kly5@mail.sfsu.edu>
2018-08-31 15:56:05 -0700

    Render components of cryptocurrency tracker but did not implement graph

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,2 +1,65 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+import Graph from './Graph.jsx';
+import Chart from 'chart';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+    };
+  }
+
+  componentDidMount() {
+    $.ajax(
+      {
+        method: 'GET',
+        url: 'https://api.coindesk.com/v1/bpi/historical/close.json',
+        error: (err) => {
+          console.log('error retrieving data', err);
+        },
+        success: (data) => {
+          console.log('successfully retrieved data', JSON.parse(data));
+          for (let i = 0; i < JSON.parse(data).bpi.length; i += 1) {
+            for (let key in JSON.parse(data).bpi){
+              const obj = {};
+              obj.date = key;
+              obj.price = JSON.parse(data).bpi[key];
+            }
+          }
+          this.setState(
+            {
+              data: JSON.parse(data),
+            },
+          );
+          this.createChart();
+        },
+      },
+    );
+  }
+
+  createChart() {
+    const elementToRenderOn = document.getElementById('chart');
+    const lineGraph = new Chart (elementToRenderOn, {
+      type: 'line',
+      data: this.state.data,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Graph />
+        <p>
+          Powered by CoinDesk 
+        </p>
+      </div>
+      );
+
+    // <Graph />
+  }
+}
+ReactDOM.render(<App />, document.getElementById('app'));
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,6 @@
+const express = require('express');
+const app = express();
 
+app.use(express.static('public'));
 
+app.listen(3000, () => console.log('App on port 3000'));
